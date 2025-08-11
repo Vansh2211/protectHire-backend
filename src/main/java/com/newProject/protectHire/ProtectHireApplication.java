65dacb79ed8bbfa246bc60a703596b0bda6047c7
Vansh <vansh.mehta@juntrax.com>
2025-08-11 13:53:14 +0530

    first commit

---
+++ src/main/java/com/newProject/protectHire/ProtectHireApplication.java
@@ -0,0 +1,13 @@
+package com.newProject.protectHire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectHireApplication.class, args);
+	}
+
+}
